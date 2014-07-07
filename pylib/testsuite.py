5e2d576442e8ba77046739ce00d84cf8fc495cf3
Robert Phillips <gtvor@comcast.net>
2014-07-07 07:05:25 +0100

    Started the regression.py script to run the tests.  The following features were added with minimal testing: 	finding of testcases 	recursive finding of testcases 	running of tests 	listing of tests to be run
    Four dummy tests were also added for testing purposes.
    The unix python script was added to set up the python path to include the
    local pylib directory before the script is run (windows version still
    pending).

--- pylib/testsuite.py
+++ pylib/testsuite.py
@@ -26,16 +26,100 @@ def find_testcases(recursive):
 def _find_testcases():
     tests = []
 
+    pwd = os.getcwd()
     if is_testcase_dir(pwd):
         tests.append(pwd)
 
-    pwd = os.getcwd()
     for item in glob.glob('*'):
-        if not os.path.isdir(item):
+        if not os.path.isdir(item) or item == 'golden':
             continue
 
         os.chdir(item)
-        test += _find_testcases()
+        tests += _find_testcases()
         os.chdir(pwd)
         
     return tests
+
+def run_script(script):
+    proc = subprocess.Popen([script])
+    proc.wait()
+
+    return (proc.returncode == 0)
+
+def has_bat_ext(path):
+    if os.path.exists(path+'setup.bat'):
+        return True
+    else:
+        return False
+        
+def write_test_summary(tc):
+    pass
+    
+class TestSuite:
+    def __init__(self, tests=[]):
+        self.testcases = []
+        self.build_testsuite(tests)
+        
+    def build_testsuite(self, tests):
+        for tc in tests:
+            self.testcases.append(TestCase(path))
+
+    def get_tests(self):
+        return self.testcases
+        
+    def add_test(self, test):
+        self.testcases.append(test)
+
+    def write_log(self, directory):
+        pwd = os.getcwd()
+        
+        os.chdir(directory)
+        write_test_summary(self.testcases)
+        os.chdir(pwd)
+
+    def run_suite(self, strict):
+        for tc in self.testcases:
+            tc.run_test(strict)
+
+        self.write_log(os.getcwd())
+        
+class TestCase:
+    def __init__(self, path, status=None):
+        self.path = path
+        self.status = None
+        self.message = ''
+
+        if has_bat_ext(path):
+            self.setup = './setup.bat'
+            self.test = './test.bat'
+            self.cleanup = './cleanup.bat'
+        else:
+            self.setup = 'setup'
+            self.test = 'test'
+            self.cleanup = 'cleanup'
+            
+    def pass_test(self):
+        self.status = True
+
+    def fail_test(self, message):
+        self.status = False
+        self.message = message
+        
+    def get_status(self):
+        return self.status
+
+    def run_test(self, strict):
+        pwd = os.getcwd()
+
+        os.chdir(self.path)
+        if not run_script(self.setup):
+            self.fail_test('Abnormal exit from \'setup\'')
+        elif not run_script(self.test):
+            self.fail_test('The \'test\' script inidicated a failure')
+        elif not run_script(self.cleanup) and strict:
+            self.fail_test('Abnormal exit from \'cleanup\'')
+        else:
+            self.pass_test()
+
+        write_test_summary([self])
+        os.chdir(pwd)
---
+++ scripts/regressions.py
@@ -0,0 +1,73 @@
+#!/bin/sh
+#
+""":"
+exec $REGRESSION_INT/wrapper/unix/python_setup $0 "$@"
+":"""
+
+# native imports
+import sys
+import os
+
+# pylib imports
+import testsuite as ts
+import commandopts as opts
+
+from testsuite import TestSuite
+
+help_string = """
+Usage: %s [ options ]
+
+    Options:
+        -h|--help      - prints this message and exits
+        -r|--recursive - recursively searches for testcases
+        -s|--strict    - any non-zero exit code will produce a failure
+                         as opposed to the default which will only
+                         produce a failure on non-zero exit codes from
+                         the setup and test scripts
+""" % sys.argv[0]
+
+# 
+
+cmdline = opts.Opts()
+cmdline.add_option('-h', alternate='--help')
+cmdline.add_option('-r', alternate='--recursive')
+cmdline.add_option('-e', alternate='--exclude')
+cmdline.add_option('-l', alternate='--list')
+
+args = cmdline.parse_args(sys.argv)
+
+recursive = False
+strict = False
+list_only = False
+
+if '-h' in args:
+    print help_string
+    sys.exit(0)
+if '-r' in args:
+    recursive = True
+if '-s' in args:
+    strict = True
+if '-e' in args:
+    # TODO: finish the exclude option
+    pass
+if '-l' in args:
+    list_only = True
+    
+tests = ts.find_testcases(recursive)
+if len(tests) == 0:
+    print 'Error: no testcases were found.'
+    sys.exit(1)
+
+if list_only:
+    base = ''
+    if recursive:
+        base = os.path.commonprefix(tests)
+        print 'Base Directory: %s\n' % base
+    
+    for tc in tests:
+        print tc.replace(base, '')
+
+    sys.exit(0)
+    
+suite = TestSuite(tests=tests)
+suite.run_suite(strict)
